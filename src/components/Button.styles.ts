668f8eb86f5dbba5779ab7a958f47153e9aa51bd
paulo <paulocesarpereira90@gmail.com>
2024-02-07 17:00:37 -0300

    Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -7,7 +7,7 @@ interface ButtonContainerProps {
 }
 
 const buttonVariants = {
-    primary: 'purple',
+    primary: '#8257e6',
     secondary: 'orange',
     danger: 'red',
     success: 'green',
@@ -17,10 +17,16 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
-    ${props => {
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white}
+
+   /* ${props => {
         return css`
         background-color: ${buttonVariants[props.variant]}
         `
-    }}
+    }} */
 `
